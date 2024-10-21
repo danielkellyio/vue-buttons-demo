f02f3334b15eb3806dc7ca0e73b44c9eb737ea28
Daniel Kelly <me@danielkelly.io>
2024-10-21 15:55:21 -0500

    home button

--- app.vue
+++ app.vue
@@ -3,6 +3,16 @@
 </script>
 
 <template>
+  <NuxtLink
+    to="/"
+    class="absolute top-1 left-1 z-10"
+  >
+    <Icon
+      name="fa-home"
+      class="w-4 h-4"
+    />
+  </NuxtLink>
+
   <div class="h-screen flex items-center justify-center">
     <NuxtPage />
   </div>
